63b400c5bd6274b09176d818481f07517e96070d
S3B4Z1MM <70818874+S3B4Z1MM@users.noreply.github.com>
2024-12-11 21:14:49 +0100

    Update path_constants.php
    deletes mistaken value

--- config/path_constants.php
+++ config/path_constants.php
@@ -3,5 +3,5 @@
 declare(strict_types = 1);
 
 define('STORAGE_PATH', __DIR__ . '/../storage');
-define('TEMPLATE_PATH', value: __DIR__ . '/../templates');
+define('TEMPLATE_PATH', __DIR__ . '/../templates');
 define('CONFIG_PATH', __DIR__ . '/../config');
